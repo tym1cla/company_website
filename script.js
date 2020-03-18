c5d85dd6d5896660d458ac47c820dd2d07685241
tym1cla <kubczaksara@gmail.com>
2020-03-18 23:01:27 +0100

    lightbox works

--- script.js
+++ script.js
@@ -2,3 +2,39 @@ $('navbar-toggler').on('click'), function () {
     $(this).toggleClass('menu-open');
 
 }
+// Animate Smooth Scroll to "Interior designs"
+$('#view1').on('click', function () {
+    const ab = $('#designs').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: ab
+        },
+        900
+    );
+});
+
+// Animate Smooth Scroll to 'Furniture'
+$('#view2').on('click', function () {
+    const ab = $('#furniture').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: ab
+        },
+        900
+    );
+});
+
+// Animate Smooth Scroll to 'Contact'
+$('#view3').on('click', function () {
+    const ab = $('#contact').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: ab
+        },
+        900
+    );
+});
+
